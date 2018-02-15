c2d1696a1086029b7ab85dc48ba31607c6813f3a
Alex Nichol <unixpickle@gmail.com>
2018-02-15 13:24:29 -0800

    formalize notion of "message"

--- connection.go
+++ connection.go
@@ -1,44 +1,13 @@
 package main
 
-const (
-	// Client messages.
-	MsgTypeLogin         = "login"
-	MsgTypeRegister      = "register"
-	MsgTypeSetPassword   = "set_password"
-	MsgTypeResetPassword = "reset_password"
-	MsgTypeLogout        = "logout"
-	MsgTypeLogoutOther   = "logout_other"
-	MsgTypeSetStatus     = "set_status"
-	MsgTypeAddBuddy      = "add_buddy"
-	MsgTypeAcceptRequest = "accept_request"
-	MsgTypeRemoveBuddy   = "remove_buddy"
-
-	// Control messages.
-	MsgTypeLoginSuccess       = "login_success"
-	MsgTypeLoginFailure       = "login_failure"
-	MsgTypeForcedLogout       = "forced_logout"
-	MsgTypeNoSuchEmail        = "no_email"
-	MsgTypeSetPasswordSuccess = "set_password_success"
-	MsgTypeSetPasswordFailure = "set_password_failure"
-
-	// State messages.
-	MsgTypeFullState       = "full_state"
-	MsgTypeRequestSent     = "request_sent"
-	MsgTypeRequestReceived = "request_received"
-	MsgTypeAcceptSent      = "accept_sent"
-	MsgTypeRequestAccepted = "request_accepted"
-	MsgTypeBuddyRemoved    = "buddy_removed"
-	MsgTypeStatusChanged   = "status_changed"
-)
-
 // A Connection communicates with a remote client in a
 // blocking manner.
 type Connection interface {
 	// ReadMessage reads a message from the remote.
-	ReadMessage() (msgType string, msgData []byte, err error)
+	ReadMessage() (message Message, err error)
 
 	// WriteMessage writes a message to the remote.
-	WriteMessage(msgType string, msgData interface{}) error
+	WriteMessage(message Message) error
 
 	// Close disconnects from the remote.
 	//
---
+++ messages.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/unixpickle/essentials"
+)
+
+const (
+	// Client messages.
+	MsgTypeLogin          = "login"
+	MsgTypeRegister       = "register"
+	MsgTypeRegisterVerify = "register_verify"
+	MsgTypeSetPassword    = "set_password"
+	MsgTypeResetPassword  = "reset_password"
+	MsgTypeLogout         = "logout"
+	MsgTypeLogoutOther    = "logout_other"
+	MsgTypeSetStatus      = "set_status"
+	MsgTypeAddBuddy       = "add_buddy"
+	MsgTypeAcceptRequest  = "accept_request"
+	MsgTypeRemoveBuddy    = "remove_buddy"
+
+	// Control messages.
+	MsgTypeLoginSuccess       = "login_success"
+	MsgTypeLoginFailure       = "login_failure"
+	MsgTypeForcedLogout       = "forced_logout"
+	MsgTypeNoSuchEmail        = "no_email"
+	MsgTypeSetPasswordSuccess = "set_password_success"
+	MsgTypeSetPasswordFailure = "set_password_failure"
+
+	// State messages.
+	MsgTypeFullState       = "full_state"
+	MsgTypeRequestSent     = "request_sent"
+	MsgTypeRequestReceived = "request_received"
+	MsgTypeAcceptSent      = "accept_sent"
+	MsgTypeRequestAccepted = "request_accepted"
+	MsgTypeBuddyRemoved    = "buddy_removed"
+	MsgTypeStatusChanged   = "status_changed"
+)
+
+// A Message is the main unit of information sent between
+// the client and the server.
+type Message interface {
+	Type() string
+}
+
+type LoginMessage struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type RegisterMessage LoginMessage
+
+type RegisterVerifyMessage struct {
+	Email string `json:"email"`
+	Token string `json:"token"`
+}
+
+type SetPasswordMessage struct {
+	Email       string `json:"email"`
+	OldPassword string `json:"old_password"`
+	NewPassword string `json:"new_password"`
+}
+
+type ResetPasswordMessage struct {
+	Email string `json:"email"`
+}
+
+type LogoutMessage struct{}
+
+type LogoutOtherMessage struct{}
+
+type SetStatusMessage struct {
+	UserStatus
+}
+
+type AddBuddyMessage ResetPasswordMessage
+
+type AcceptRequestMessage ResetPasswordMessage
+
+type RemoveBuddyMessage ResetPasswordMessage
+
+type LoginSuccessMessage struct{}
+
+type LoginFailureMessage struct {
+	Message string `json:"message"`
+}
+
+type ForcedLogoutMessage struct{}
+
+func (*LoginMessage) Type() string {
+	return MsgTypeLogin
+}
+
+func (*RegisterMessage) Type() string {
+	return MsgTypeRegister
+}
+
+func (*RegisterVerifyMessage) Type() string {
+	return MsgTypeRegisterVerify
+}
+
+func (*SetPasswordMessage) Type() string {
+	return MsgTypeSetPassword
+}
+
+func (*ResetPasswordMessage) Type() string {
+	return MsgTypeResetPassword
+}
+
+func (*LogoutMessage) Type() string {
+	return MsgTypeLogout
+}
+
+func (*LogoutOtherMessage) Type() string {
+	return MsgTypeLogoutOther
+}
+
+func (*SetStatusMessage) Type() string {
+	return MsgTypeSetStatus
+}
+
+func (*AddBuddyMessage) Type() string {
+	return MsgTypeAddBuddy
+}
+
+func (*AcceptRequestMessage) Type() string {
+	return MsgTypeAcceptRequest
+}
+
+func (*RemoveBuddyMessage) Type() string {
+	return MsgTypeRemoveBuddy
+}
+
+func (*LoginSuccessMessage) Type() string {
+	return MsgTypeLoginSuccess
+}
+
+func (*LoginFailureMessage) Type() string {
+	return MsgTypeLoginFailure
+}
+
+func (*ForcedLogoutMessage) Type() string {
+	return MsgTypeForcedLogout
+}
+
+// DecodeMessage decodes a message into its Go type.
+func DecodeMessage(msgType string, data []byte) (msg Message, err error) {
+	defer essentials.AddCtxTo("decode message", &err)
+	mapping := map[string]Message{
+		MsgTypeLogin:          &LoginMessage{},
+		MsgTypeRegister:       &RegisterMessage{},
+		MsgTypeRegisterVerify: &RegisterVerifyMessage{},
+		MsgTypeSetPassword:    &SetPasswordMessage{},
+		MsgTypeResetPassword:  &ResetPasswordMessage{},
+		MsgTypeLogout:         &LogoutMessage{},
+		MsgTypeLogoutOther:    &LogoutOtherMessage{},
+		MsgTypeSetStatus:      &SetStatusMessage{},
+		MsgTypeAddBuddy:       &AddBuddyMessage{},
+		MsgTypeAcceptRequest:  &AcceptRequestMessage{},
+		MsgTypeRemoveBuddy:    &RemoveBuddyMessage{},
+		MsgTypeLoginSuccess:   &LoginSuccessMessage{},
+		MsgTypeLoginFailure:   &LoginFailureMessage{},
+		MsgTypeForcedLogout:   &ForcedLogoutMessage{},
+	}
+	if obj, ok := mapping[msgType]; ok {
+		if err := json.Unmarshal(data, obj); err != nil {
+			return nil, err
+		}
+		return obj, nil
+	} else {
+		return nil, errors.New("unknown message type: " + msgType)
+	}
+}
